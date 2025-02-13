e954d4ca58dfe2d69d9f347b793cfb594fa6338b
DESKTOP-65SAUST\Alumno <informatica@esne.es>
2025-02-13 15:50:53 +0100

    basic styles and isotope

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function()
+{
+    let $pokeCont = $(".poke-cont");
+
+    $pokeCont.isotope(
+    {
+        itemSelector: ".card",
+        layoutMode: "fitRows"
+    });
+
+    $(".btns-cont button").on("click", function()
+    {
+        let filterVal = $(this).data("filter");
+        $pokeCont.isotope(
+        {
+            filter: filterVal
+        })
+    });
+
+    // INSTRUCCIONES:
+
+    // Botón "Todos"
+
+    // Botones de orden: por número y orden alfabético
+});
